f5a24af04ab62065f4dc3b3feec83fae0ea608e0
Nickyshe <judypearls200@gmail.com>
2023-10-10 05:52:43 +0100

    Passing data navigation

--- app/src/main/java/com/example/navigationcompose/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/navigationcompose/navigation/AppNavigation.kt
@@ -1,2 +1,39 @@
 package com.example.navigationcompose.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.navigationcompose.ui.theme.ui.Screen1
+import com.example.navigationcompose.ui.theme.ui.Screen2
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "screen_1") {
+        composable("screen_1") {
+            Screen1(navController)
+        }
+        composable(
+            "screen_2/{name}/{age}",
+            arguments = listOf(
+                navArgument(name = "name") {
+                    type = NavType.StringType
+                },
+                navArgument(name = "age") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backstackEntry ->
+            Screen2(
+                name = backstackEntry.arguments?.getString("name"),
+                age = backstackEntry.arguments?.getInt("age")
+            )
+        }
+
+    }
+}
+
+
--- app/src/main/java/com/example/navigationcompose/ui/theme/ui/Screen2.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/ui/Screen2.kt
@@ -1,2 +1,40 @@
 package com.example.navigationcompose.ui.theme.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Screen2(
+    name: String?,
+    age: Int?
+){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Screen 2 Details",
+            fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(text = "My name is: $name",
+            fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(text = "My age is: $age",
+            fontSize = 20.sp)
+
+
+    }
+}
+
+
